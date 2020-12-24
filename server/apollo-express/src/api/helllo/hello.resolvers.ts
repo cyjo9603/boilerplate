93bbdbf493f495253463f3c32694ea2ce5ba591f
chanyeong <cyjo9603@naver.com>
2020-12-24 13:14:13 +0900

    Chore #1 : apollo-server 설정
    - schema 분기 설정
    - apollo-server 와 express 연결
    - 초기 query 작성

---
+++ server/apollo-express/src/api/helllo/hello.resolvers.ts
@@ -0,0 +1,7 @@
+const resovlers = {
+  Query: {
+    hello: () => ({ message: 'hello apollo!' }),
+  },
+};
+
+export default resovlers;
--- server/apollo-express/src/app.ts
+++ server/apollo-express/src/app.ts
@@ -1,3 +1,4 @@
+import { ApolloServer, CorsOptions } from 'apollo-server-express';
 import express from 'express';
 import helmet from 'helmet';
 import hpp from 'hpp';
@@ -5,27 +6,46 @@ import compression from 'compression';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
 
+import schema from '@config/schema';
+
+const GRPAHQL_ENDPOINT = '/graphql';
 const prod = process.env.NODE_ENV === 'production';
 
 class App {
   public app;
 
+  private apollo;
+
   constructor() {
     this.app = express();
+    this.apollo = new ApolloServer({
+      context: ({ req, res }) => ({ req, res }),
+      playground: !prod,
+      schema,
+    });
     this.middlewares();
   }
 
   private middlewares() {
+    const corsOptions: CorsOptions = { credentials: true };
     if (prod) {
       this.app.use(helmet());
       this.app.use(hpp());
       this.app.use(logger('combined'));
+      corsOptions.origin = /user-domain\.com$/;
     } else {
       this.app.use(logger('dev'));
+      corsOptions.origin = true;
     }
 
     this.app.use(cookieParser());
     this.app.use(compression());
+
+    this.apollo.applyMiddleware({
+      app: this.app,
+      path: GRPAHQL_ENDPOINT,
+      cors: corsOptions,
+    });
   }
 }
 
