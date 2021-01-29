6efcbf1d690ebc354e01d27b46f51be3669ce481
chanyeong <cyjo9603@naver.com>
2021-01-29 21:14:24 +0900

    Chore #4 : 프로젝트 절대 경로 설정
    - craco를 사용한 프로젝트 절대 경로 설정

---
+++ client/react-antd/craco.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+const cracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+};
