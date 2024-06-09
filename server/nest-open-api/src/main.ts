3b046ca2b816ed89464abcd53f3f7c23fa6b85b9
cyjo9603 <cyjo9603@naver.com>
2024-06-09 16:23:33 +0900

    Chore: #5 init Default Server Config

--- server/nest-open-api/src/main.ts
+++ server/nest-open-api/src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
+import helmet from 'helmet';
+import * as hpp from 'hpp';
+import * as csurf from 'csurf';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  app.use(cookieParser());
+  app.use(compression());
+  if (configService.get('appEnv') !== 'development') {
+    app.use(hpp());
+    app.use(helmet());
+    app.use(csurf());
+  }
 
   await app.listen(configService.get('port'), '0.0.0.0');
 
